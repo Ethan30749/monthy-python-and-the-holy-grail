2d6d5e6b4ca34c5a3427d67672c3c7977643c20f
Ethan30749 <56318079+Ethan30749@users.noreply.github.com>
2020-03-03 16:00:22 -0300

    Update practica suma y promedio lista.py

--- practica suma y promedio lista.py
+++ practica suma y promedio lista.py
@@ -1,12 +1,22 @@
+def Suma(Lista):
+    suma=0
+    for i in range(len(Lista)):
+        suma+=Lista[i]
+    return suma
+
+def Promedio(Lista):
+    suma = Suma(Lista)
+    N = len(Lista)
+    return suma / N
+
 Lista=[]
 M=int(input("ingrese cantidad de valores: "))
-suma=0
-promedio=0
 for i in range(M):
-    N=int(input("ingrese valores: "))
-    Lista.append(N)
-for i in range (1, len(Lista)):
-    suma+=Lista[i]
-promedio=suma/len(Lista)
-print("suma total de valores en la lista: ", suma)
+    Valor=int(input("ingrese valores: "))
+    Lista.append(Valor)
+    
+sumatoria= Suma(Lista)
+promedio= Promedio(Lista)
+
+print("suma total de valores en la lista: ", sumatoria)
 print("promedio de los valores en la lista: ", promedio)
