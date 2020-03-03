06eb3bfd1f6a2e9a89fdd164a647ea7716891095
Ethan30749 <56318079+Ethan30749@users.noreply.github.com>
2020-03-03 14:42:50 -0300

    Update Creador de matrices.py

--- Creador de matrices.py
+++ Creador de matrices.py
@@ -1,11 +1,16 @@
 def crear_matriz(M,N):
     matriz= []
+    #atribuye al valor "matriz" una lista vacía
     for i in range(M):
         matriz.append([])
+        #agrega listas vacías a la matriz equivalente al valor M
         for j in range (N):
             value=int(input("ingrese valores: "))
             matriz[i].append(value)
+            #agrega N valores a cada lista de la matriz
     return matriz
-F=int(input("ingrese cantidad de filas: "))
-C=int(input("ingrese cantidad de columnas: "))
-print(crear_matriz(F,C))
+    #devuelve la matriz completa
+M=int(input("ingrese cantidad de filas: "))
+N=int(input("ingrese cantidad de columnas: "))
+#solicita los valores M y N para construir la matriz
+print(crear_matriz(M,N))
